8ba19097016054e5ee52207582f1d6fdafc84414
shiyad0344 <saca0344@gmail.com>
2026-02-15 05:45:12 +0530

    vercel config

--- server/app.js
+++ server/app.js
@@ -11,6 +11,10 @@ app.use(cors({
 app.use(express.json({limit:'16kb'}));
 app.use(express.urlencoded({extended:true,limit:'16kb'}));
 
+app.get("/", (req, res) => {
+  res.json({ message: "Backend is running " });
+});
+
 
 import extractRoute from './routes/extract.route.js';
 app.use("/api/extract",extractRoute)
